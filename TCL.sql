3b94dc6b32245e248b7b279b0c0733beb708f425
SARAVANAN M <158152535+Saravanan-mc@users.noreply.github.com>
2025-12-28 09:39:28 +0530

    Add employees table and transaction operations
    Created employees table and added data manipulation operations.

--- TCL.sql
+++ TCL.sql
@@ -1,37 +1,64 @@
-CREATE TABLE student (
-    student_id INT,
-    student_name VARCHAR(50),
-    age INT,
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
     department VARCHAR(50),
-    email VARCHAR(100)
+    salary DECIMAL(10,2),
+    hire_date DATE
 );
 
--- Turn OFF auto commit
+
+INSERT INTO employees (emp_id, emp_name, department, salary, hire_date) VALUES
+(101, 'John Smith', 'HR', 45000.00, '2018-06-15'),
+(102, 'Alice Brown', 'IT', 65000.00, '2019-09-10'),
+(103, 'Michael Johnson', 'Finance', 60000.00, '2017-03-25'),
+(104, 'Sophia Davis', 'Marketing', 50000.00, '2020-01-05'),
+(105, 'David Wilson', 'IT', 72000.00, '2016-11-18'),
+(106, 'Emma Taylor', 'HR', 48000.00, '2019-02-12');
+
+
+--️ Turn off auto-commit
 SET autocommit = 0;
 
--- Start transaction (optional in MySQL)
+-- Start a transaction
 START TRANSACTION;
 
--- Insert data
-INSERT INTO student VALUES (1, 'Rahul', 20, 'CS', 'rahul@gmail.com');
+-- Set transaction properties (optional)
+SET TRANSACTION READ WRITE, ISOLATION LEVEL READ COMMITTED;
 
--- Create a savepoint
-SAVEPOINT sp1;
+--️ Perform DML operations
+INSERT INTO employees (emp_id, emp_name, department, salary, hire_date)
+VALUES (601, 'Sophia', 'IT', 72000, '2025-01-15');
 
--- Insert another record
-INSERT INTO student VALUES (2, 'Anita', 21, 'IT', 'anita@gmail.com');
+SAVEPOINT sp1;  -- create a savepoint
 
--- Rollback to savepoint (undo second insert only)
-ROLLBACK TO sp1;
+UPDATE employees SET salary = 75000 WHERE emp_id = 601;
 
--- Update data
-UPDATE student SET age = 21 WHERE student_id = 1;
+--  Rollback to savepoint if needed
+ROLLBACK TO sp1;  -- undo salary update but keep insert
 
--- Commit the transaction (save permanently)
-COMMIT;
+-- Commit the transaction
+COMMIT;  -- save changes permanently
 
--- Turn ON auto commit
+--  Turn auto-commit back on
 SET autocommit = 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
